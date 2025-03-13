a56729e95c3da0b76cc99fd8d927e0963e179edd
Paul-Loup-Germain <paulloup.germain@etml-es.ch>
2025-03-13 09:15:28 +0100

    Add test file

---
+++ JavaFXComputersApplication/src/test/java/ch/etmles/srs/buisness/ComputerTest.java
@@ -0,0 +1,16 @@
+package ch.etmles.srs.buisness;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerTest {
+
+    @Test
+    void getCard() {
+    }
+
+    @Test
+    void setCard() {
+    }
+}
---
+++ JavaFXComputersApplication/src/test/java/ch/etmles/srs/buisness/NetworkCardTest.java
@@ -0,0 +1,15 @@
+package ch.etmles.srs.buisness;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class NetworkCardTest {
+
+    @Test
+    void getSubNetAddress() {
+    }
+
+    @Test
+    void getBroadcastAddress() {
+    }
+}
